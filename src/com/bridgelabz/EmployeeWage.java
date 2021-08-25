545908cd206bb5b1d58fca78c2408f8f7bc15baf
balachandar <govindalapudibalachandar@gmail.com>
2021-08-25 22:33:07 +0530

    Added:employeedailywage file is added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,25 +2,49 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	
-	int IS_FULL_TIME;
+	int IS_FULL_TIME=1;
+	double empcheck;
 	
 	public void attendence()
 	{
-	double empcheck = Math.floor(Math.random() * 10) %2;
+	//int IS_FULL_TIME=1;
+
+	 empcheck = Math.floor(Math.random() * 10) %2;
+   
 	if (empcheck == IS_FULL_TIME)
-	{
+	
 	System.out.println("Employee Is Present");
-	}
+	
 	else
 	{
 		System.out.println("Employee Is Absent");
 	}
+	
+	}
+     public void dailywage () {
+    	 
+	  int IS_RATE_PER_HOUR =20;
+     int emphr= 0;
+     int empwage = 0;
+     
+	if(empcheck == IS_FULL_TIME) 
+	
+	{
+		emphr=8;
+		}
+	else
+	{
+			emphr=0;
+	}
+	empwage = emphr * IS_RATE_PER_HOUR;
+	System.out.println("Emp wage :"+empwage);
 	}
 
 	public static void main(String[] args) {
 		EmployeeWage obj = new EmployeeWage();
-		obj.attendence();
-
+		 obj.attendence();
+		 EmployeeWage obj1 = new EmployeeWage();
+         obj.dailywage();
 	}
 
 }

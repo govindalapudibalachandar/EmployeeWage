3f98590b7a228abd05c66bdf86e7b5217348c902
balachandar <govindalapudibalachandar@gmail.com>
2021-08-26 12:47:18 +0530

    Added:empswitchcase file is added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,15 +2,15 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	
-	int IS_FULL_TIME=1;
-	//int  IS_PART_TIME= 2;	
-	double empcheck;
+	public static final int IS_FULL_TIME=1;
+	public static final int  IS_PART_TIME= 2;	
+	int empcheck;
 	
 	public void attendence()
 	{
 	//int IS_FULL_TIME=1;
 
-	 empcheck = Math.floor(Math.random() * 10) %3;
+	  empcheck = (int) Math.floor(Math.random() * 10) %3;
    
 	if (empcheck == IS_FULL_TIME)
 	
@@ -24,22 +24,20 @@ public class EmployeeWage {
 	}
      public void empdaily() 
      {
-     int IS_PART_TIME= 2;	 
+   // public static final int IS_PART_TIME= 2;	 
 	  int IS_RATE_PER_HOUR =20;
      int emphr= 0;
      int empwage = 0;
      
-	if(empcheck == IS_PART_TIME)
-	{
+     switch (empcheck) {
+     case IS_PART_TIME:
 		emphr = 4;
-	}
-	else if(empcheck == IS_FULL_TIME)
-	{
+		break;
+     case IS_FULL_TIME:
 		emphr=8;
-		}
-	else
-	{
-			emphr=0;
+		break;
+	 default:
+		emphr=0;
 	}
 	empwage = emphr * IS_RATE_PER_HOUR;
 	System.out.println("Emp wage :"+empwage);
@@ -48,7 +46,6 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		 EmployeeWage obj = new EmployeeWage();
 		 obj.attendence();
-	   //  EmployeeWage obj1 = new EmployeeWage();
          obj.empdaily();
       
 	}

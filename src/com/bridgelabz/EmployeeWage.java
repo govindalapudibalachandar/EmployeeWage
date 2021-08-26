78f97e0f076594c7cdcffc7c51df42c268d3bb9b
balachandar <govindalapudibalachandar@gmail.com>
2021-08-26 13:25:11 +0530

    Added:Empwageformonth file is added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,32 +3,29 @@ package com.bridgelabz;
 public class EmployeeWage {
 	
 	public static final int IS_FULL_TIME=1;
-	public static final int  IS_PART_TIME= 2;	
+	public static final int  IS_PART_TIME= 2;
+	public static final int NUM_OF_WORKING_DAYS =2;
 	int empcheck;
 	
 	public void attendence()
 	{
-	//int IS_FULL_TIME=1;
-
 	  empcheck = (int) Math.floor(Math.random() * 10) %3;
    
 	if (empcheck == IS_FULL_TIME)
-	
 	System.out.println("Employee Is Present");
-	
 	else
 	{
 		System.out.println("Employee Is Absent");
-	}
-	
+	}	
 	}
      public void empdaily() 
      {
-   // public static final int IS_PART_TIME= 2;	 
+   
 	  int IS_RATE_PER_HOUR =20;
      int emphr= 0;
-     int empwage = 0;
-     
+     int empwage = 0,totalempwage =0;
+  //   switch (empcheck) {
+     for(int day =0; day < NUM_OF_WORKING_DAYS; day++) {
      switch (empcheck) {
      case IS_PART_TIME:
 		emphr = 4;
@@ -40,7 +37,10 @@ public class EmployeeWage {
 		emphr=0;
 	}
 	empwage = emphr * IS_RATE_PER_HOUR;
+    totalempwage += empwage;
 	System.out.println("Emp wage :"+empwage);
+     }
+     System.out.println("Total Emp Wage :"+totalempwage);
 	}
 
 	public static void main(String[] args) {

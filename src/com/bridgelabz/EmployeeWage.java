a2b73ed8f69b1710effd665a12d5cf0d21f59322
balachandar <govindalapudibalachandar@gmail.com>
2021-08-26 11:45:02 +0530

    Added: emppartime file is added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,13 +3,14 @@ package com.bridgelabz;
 public class EmployeeWage {
 	
 	int IS_FULL_TIME=1;
+	//int  IS_PART_TIME= 2;	
 	double empcheck;
 	
 	public void attendence()
 	{
 	//int IS_FULL_TIME=1;
 
-	 empcheck = Math.floor(Math.random() * 10) %2;
+	 empcheck = Math.floor(Math.random() * 10) %3;
    
 	if (empcheck == IS_FULL_TIME)
 	
@@ -21,14 +22,18 @@ public class EmployeeWage {
 	}
 	
 	}
-     public void dailywage () {
-    	 
+     public void empdaily() 
+     {
+     int IS_PART_TIME= 2;	 
 	  int IS_RATE_PER_HOUR =20;
      int emphr= 0;
      int empwage = 0;
      
-	if(empcheck == IS_FULL_TIME) 
-	
+	if(empcheck == IS_PART_TIME)
+	{
+		emphr = 4;
+	}
+	else if(empcheck == IS_FULL_TIME)
 	{
 		emphr=8;
 		}
@@ -41,10 +46,11 @@ public class EmployeeWage {
 	}
 
 	public static void main(String[] args) {
-		EmployeeWage obj = new EmployeeWage();
+		 EmployeeWage obj = new EmployeeWage();
 		 obj.attendence();
-		 EmployeeWage obj1 = new EmployeeWage();
-         obj.dailywage();
+	   //  EmployeeWage obj1 = new EmployeeWage();
+         obj.empdaily();
+      
 	}
 
 }

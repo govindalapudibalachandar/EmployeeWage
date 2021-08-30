a81457278d5b41c4ffaf7560d34efcc274b77193
balachandar <govindalapudibalachandar@gmail.com>
2021-08-30 15:17:43 +0530

    Added eachcompany file is added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,12 +5,24 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME=1;
 	public static final int  IS_PART_TIME= 2;
 	
-	public static int computeEmpWage (String company,int empRatePerHour, int numOfWorkingDay ,int maxHourPerMonth)
+	private final String company ;
+	private final int empRatePerHour;
+	private final int numOfWorkingDay;
+	private final int maxHourPerMonth;
+	private int totalEmpWage;
+	public EmployeeWage(String company,int empRatePerHour,int numOfWorkingDay,int maxHourPerMonth) {
+	 
+		this.company = company ;
+		this.empRatePerHour = empRatePerHour ;
+		this.numOfWorkingDay = numOfWorkingDay ;
+		this.maxHourPerMonth = maxHourPerMonth ;
+	}
+	public void computeEmpWage ()
 	{
 		//variables
-				int empHrs =0, totalemphr=0, totalworkingdays=0 ;
+				int empHrs =0, totalEmphrs=0, totalworkingdays=0,totalempwage =0;
 			
-		  int empwage = 0,totalempwage=0, totalEmphrs=0 ;
+     	 // int empwage = 0, totalEmphrs=0  ;
 		  {
 		  while (totalEmphrs <= maxHourPerMonth && totalworkingdays < numOfWorkingDay) {
 			  totalworkingdays++;
@@ -29,12 +41,23 @@ public class EmployeeWage {
 		    System.out.println("Days# " + totalworkingdays +"Emp hr :"+empHrs );
 		  }
 			int totalEmpWage = totalEmphrs * empRatePerHour;
-		     System.out.println("Total Emp Wage for company  :" + company+"is :" +totalEmpWage);	
-		     return totalempwage ;}
-         }
+		  }
+	}
+		// @Override
+		 public String tostring()
+		  {
+		  
+		     return "Total Emp Wage for company  :" +company+ "is :"+ totalEmpWage;	
+		  }
+
 		  public static void main(String[] args) {
-		 computeEmpWage("Dmart",20,2,10);
-         computeEmpWage("Reliance", 10,4,20);     
+		 EmployeeWage dMart = new EmployeeWage ("Dmart",20,2,10);
+         EmployeeWage reliance = new EmployeeWage ("Reliance", 10,4,20);     
+         dMart.computeEmpWage();
+         System.out.println(dMart);
+         reliance.computeEmpWage();
+         System.out.println(reliance);
     }
 }
 
+ 
